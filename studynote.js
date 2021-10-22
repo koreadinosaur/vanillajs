e9d1c41f490f5f7157569a86a41ee883c18c8e13
koreadinosaur <91491029+koreadinosaur@users.noreply.github.com>
2021-10-22 15:12:38 +0900

    js 3.6-3.8 note

--- studynote.js
+++ studynote.js
@@ -113,3 +113,115 @@ console.log(addcalculrator)
 2.13
 const age = prompt("how old are you?")
 console.log(age);
+
+----------------------------------------------------
+3.0-3.5
+
+const menu = document.querySelector(".menu h1:nth-child(1)")
+function clickevent(){
+  console.log("it was clicked");
+  menu.style.color = "blue"  
+};
+function mouseenterevent(){
+    menu.style.border = '5px dotted orange';
+}
+function mouseleave(){
+    menu.style = null;
+}
+
+function HandleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+
+menu.addEventListener("click", clickevent)
+menu.addEventListener("mouseenter", mouseenterevent)
+menu.addEventListener("mouseleave", mouseleave)
+
+window.onresize = HandleWindowResize;
+
+document.get
+
+
+
+const title = document.querySelector("h2");//class나 id 없어도 태그만으로도 호출가능//
+const superEventHandler = {
+  superEventHandlerEnter: function () {
+    title.innerText = "Thank you Lynn";
+    title.style.color = "#1abc9c";
+  },
+  superEventHandlerLeave: function () {
+    title.innerText = "Thank you nicolas";
+    title.style.color = "#3498db";
+  },
+  superEventHandlerResize: function () {
+    title.innerText = "Have a nice day";
+    title.style.color = "#9b59b6";
+  },
+  superEventHandlerRightClick: function () {
+    title.innerText = "why do you click right?";
+    title.style.color = "#f39c12";
+  }
+};
+title.addEventListener("mouseenter", superEventHandler.superEventHandlerEnter);
+title.addEventListener("mouseleave", superEventHandler.superEventHandlerLeave);
+window.addEventListener("resize", superEventHandler.superEventHandlerResize);
+window.addEventListener(
+  "contextmenu",
+  superEventHandler.superEventHandlerRightClick
+);
+-------------------------------------------------------
+function clickevent(){
+    const currentColor = menu.style.color;
+    let newColor;
+    if (currentColor === "blue") {
+    newColor = "tomato";  
+    } else { 
+    newColor = "blue";
+    }
+    menu.style.color = newColor;
+  };
+  
+  menu.addEventListener("click", clickevent)
+
+
+  function clickEvent() {
+    const clickedClass = "active"
+    if (menu.classList.contains(clickedClass)) {
+      menu.classList.remove(clickedClass);
+    } else {
+      menu.classList.add(clickedClass);
+    } //기존의 class를 삭제하지 않고 active를 추가할 수 있는 방법"
+-----------------------------------------------------------------
+assignment
+https://codesandbox.io/s/empty-blueprint-forked-0ekww?file=/src/index.js
+    const bodyColor = document.querySelector("body");
+
+    //이건 js파일//
+function colorChange() {
+  const viewportWidth = window.innerWidth;
+  if (viewportWidth >= 800) {
+    bodyColor.classList.remove("medium", "small");
+    bodyColor.classList.add("large");
+  } else if (viewportWidth >= 600 && viewportWidth < 800) {
+    bodyColor.classList.remove("large", "small");
+    bodyColor.classList.add("medium");
+  } else if (viewportWidth<600) {
+    bodyColor.classList.remove("medium", "large");
+    bodyColor.classList.add("small");
+  }
+}
+window.addEventListener("resize", colorChange)
+
+//css파일//
+.large {
+    background-color: red;
+  }
+  
+  .medium {
+    background-color: blue;
+  }
+  .small {
+    background-color: green;
+  }
+  //처음에 bodyColor.style.backgroundcolor: red; 이렇게 해서 됐었는데
+  //body하나 쓰고 지우니까 갑자기 안됨. 찾아보니까 background-color로 하네...
